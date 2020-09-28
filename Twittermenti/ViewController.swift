4b67a0424130392b30673f92454d74bdb4d7f77f
aglegaspi <lex83nyc@gmail.com>
2020-09-28 09:37:53 -0400

    add comments

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -35,20 +35,33 @@ class ViewController: UIViewController {
         let prediction = try! sentimentClassifier.prediction(text: "@apple is a great company")
         print(prediction.label)
         
-        swifter.searchTweet(using: "@apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+        
+        swifter.searchTweet(using: "@apple", // <-- this will change to be set to the user input from textField
+                            lang: "en", // only return English results to this API call
+                            count: 100, // the aount of results we expect from the search
+                            tweetMode: .extended, // allows us to retreive the full text from the tweet
+                            success: { (results, metadata) in
             
             // set this array to TSCI objects to pass in the classifers predictions expected type
             var tweets = [TweetSentimentClassifierInput]()
             
             for i in 0..<100 {
                 if let tweet = results[i]["full_text"].string {
+                    
+                    // turns each tweet into the expected type for the array in order to append
                     let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
                     tweets.append(tweetForClassification)
                 }
             }
             do {
+                
+                // pass in array of TweetSentimentClassifierInput objects and if successful store data in predictions array
                 let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
-                print(predictions[0].label)
+                
+                // loops through the predictions array and on each prediction objecet print the value of the label property
+                for prediction in predictions {
+                    print(prediction.label)
+                }
             } catch {
                 print("Error making prediction: \(error)")
             }
