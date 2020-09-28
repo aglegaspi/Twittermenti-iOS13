f94f952f0b9e62e80c9ba0412f4f45a3f6de0f91
aglegaspi <lex83nyc@gmail.com>
2020-09-28 10:17:57 -0400

    refactor to split off functionality and create Constants struct

---
+++ Twittermenti/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Twittermenti
+//
+//  Created by Alex 6.1 on 9/28/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let tweetCount = 100
+}
--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -42,20 +42,24 @@ class ViewController: UIViewController {
     //MARK: - METHODS
     
     @IBAction func predictPressed(_ sender: Any) {
+        fetchTweets()
+    }
+    
+    func fetchTweets() {
         if let searchText = textField.text {
             let prediction = try! sentimentClassifier.prediction(text: "@apple is a great company")
             print(prediction.label)
             
             swifter.searchTweet(using: searchText, // user input
                                 lang: "en", // only return English results to this API call
-                                count: 100, // the aount of results we expect from the search
+                                count: K.tweetCount, // the aount of results we expect from the search
                                 tweetMode: .extended, // allows us to retreive the full text from the tweet
                                 success: { (results, metadata) in
                                     
                                     // set this array to TSCI objects to pass in the classifers predictions expected type
                                     var tweets = [TweetSentimentClassifierInput]()
                                     
-                                    for i in 0..<100 {
+                                    for i in 0..<K.tweetCount {
                                         if let tweet = results[i]["full_text"].string {
                                             
                                             // turns each tweet into the expected type for the array in order to append
@@ -63,49 +67,56 @@ class ViewController: UIViewController {
                                             tweets.append(tweetForClassification)
                                         }
                                     }
-                                    do {
-                                        
-                                        // pass in array of TweetSentimentClassifierInput objects and if successful store data in predictions array
-                                        let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
-                                        
-                                        var sentimentScore = 0
-                                        
-                                        // loops through the predictions array and on each prediction objecet print the value of the label property
-                                        for prediction in predictions {
-                                            let sentiment = prediction.label
-                                            
-                                            if sentiment == "Pos" {
-                                                sentimentScore += 1
-                                            } else if sentiment == "Neg" {
-                                                sentimentScore -= 1
-                                            }
-                                        }
-                                        
-                                        if sentimentScore > 20 {
-                                            self.sentimentLabel.text = "😍"
-                                        } else if sentimentScore > 10 {
-                                            self.sentimentLabel.text = "😀"
-                                        } else if sentimentScore > 0 {
-                                            self.sentimentLabel.text = "🙂"
-                                        } else if sentimentScore == 0 {
-                                            self.sentimentLabel.text = "😐"
-                                        } else if sentimentScore > -10 {
-                                            self.sentimentLabel.text = "😕"
-                                        }  else if sentimentScore > -20 {
-                                            self.sentimentLabel.text = "😡"
-                                        } else {
-                                            self.sentimentLabel.text = "🤮"
-                                        }
-                                        
-                                    } catch {
-                                        print("Error making prediction: \(error)")
-                                    }
                                     
-                            
-                                    //print(results)
+                                    self.makePrediction(with: tweets)
+                
                                 }) { (error) in
                 print("Error w/Twitter API request: \n \(error)")
             }
+        }
+    }
+    
+    func makePrediction(with tweets: [TweetSentimentClassifierInput]) {
+        do {
+            
+            // pass in array of TweetSentimentClassifierInput objects and if successful store data in predictions array
+            let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+            
+            var sentimentScore = 0
+            
+            // loops through the predictions array and on each prediction objecet print the value of the label property
+            for prediction in predictions {
+                let sentiment = prediction.label
+                
+                if sentiment == "Pos" {
+                    sentimentScore += 1
+                } else if sentiment == "Neg" {
+                    sentimentScore -= 1
+                }
+            }
+            
+            self.updateUI(with: sentimentScore)
+            
+        } catch {
+            print("Error making prediction: \(error)")
+        }
+    }
+    
+    func updateUI(with sentimentScore: Int) {
+        if sentimentScore > 20 {
+            self.sentimentLabel.text = "😍"
+        } else if sentimentScore > 10 {
+            self.sentimentLabel.text = "😀"
+        } else if sentimentScore > 0 {
+            self.sentimentLabel.text = "🙂"
+        } else if sentimentScore == 0 {
+            self.sentimentLabel.text = "😐"
+        } else if sentimentScore > -10 {
+            self.sentimentLabel.text = "😕"
+        }  else if sentimentScore > -20 {
+            self.sentimentLabel.text = "😡"
+        } else {
+            self.sentimentLabel.text = "🤮"
         }
     }
     
